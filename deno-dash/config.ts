85df8dbf59b8c613fa4587371e10f82410b81411
fp-58 <92231137+fp-58@users.noreply.github.com>
2025-01-22 19:42:06 -0500

    deno-dash: add filter

---
+++ deno-dash/config.ts
@@ -0,0 +1,67 @@
+import defaultConfig from "./defaults.json" with { type: "json" };
+import { readJsonFile } from "./utils.ts";
+
+export async function loadFilterConfig(
+  configFile: string,
+): Promise<typeof defaultConfig> {
+  try {
+    const config = await readJsonFile(configFile);
+    if (typeof config !== "object" || Array.isArray(config)) {
+      console.warn("Invalid config file: root object is not an object.");
+    } else {
+      for (const key in defaultConfig) {
+        if (!(key in config)) {
+          config[key] = defaultConfig[key as keyof typeof defaultConfig];
+        }
+      }
+      return config;
+    }
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      console.warn(`Missing config file at ${configFile}`);
+    } else {
+      throw err;
+    }
+  }
+
+  console.warn(`Using default configuration.`);
+  return defaultConfig;
+}
+
+export function checkProjectConfig(config: object) {
+  if (!("compiler" in config)) {
+    console.error('Missing "compiler" property in project config.');
+    return false;
+  }
+
+  return true;
+}
+
+// deno-lint-ignore no-explicit-any
+export function prepareProjectConfig(projectConfig: any) {
+  if ("behaviorPack" in projectConfig.packs) {
+    projectConfig.packs.behaviorPack = "./BP";
+  }
+  if ("resourcePack" in projectConfig.packs) {
+    projectConfig.packs.resourcePack = "./RP";
+  }
+
+  // deno-lint-ignore no-explicit-any
+  const plugins: (string | [string, any])[] = projectConfig.compiler.plugins;
+  for (let i = 0; i < plugins.length; i++) {
+    if (Array.isArray(plugins[i])) {
+      switch (plugins[i][0]) {
+        case "simpleRewrite":
+          plugins[i][1].packName = "project";
+          delete plugins[i][1].packNameSuffix;
+          break;
+        case "rewriteForPackaging":
+          console.warn('Dash plugin "rewriteForPackaging" will be ignored.');
+          plugins.splice(i--, 1);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
